1ac362d3092fc28d35e6ff1ec01dead25da2284c
ByteBlast <byteblast@gmail.com>
2014-07-07 21:43:11 +0100

    Euler 2.

---
+++ euler/2-even-fibonacci-numbers.py
@@ -0,0 +1,13 @@
+sequence  = [0, 1]
+sum =0
+
+while True:
+    result = sequence[0] + sequence[1]
+    if result > 4000000:
+        break
+    if (result % 2 == 0):
+        sum += result;
+
+    sequence[0] = sequence[1]
+    sequence[1] = result
+print sum

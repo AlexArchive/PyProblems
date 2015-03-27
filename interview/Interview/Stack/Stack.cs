b18321d76853ceea7f82b0e0d510b3c6767e653a
ByteBlast <byteblast@gmail.com>
2015-03-27 21:36:52 +0000

    Added stack interview question

---
+++ interview/Interview/Stack/Stack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Interview.Stack
+{
+    public class Stack
+    {
+        public int Count { get; private set; }
+        public int Capacity { get { return 10; } }
+
+        private readonly int[] elements = new int[10];
+
+        public void Push(int num)
+        {
+            if (Count == Capacity) throw new InvalidOperationException();
+
+            elements[Count] = num;
+            Count += 1;
+
+        }
+
+        public int Pop()
+        {
+            if (Count == 0) throw new InvalidOperationException();
+
+            Count -= 1;
+            var element = elements[Count];
+            return element;
+        }
+    }
+}
---
+++ interview/Interview/Stack/StackFacts.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Xunit;
+
+namespace Interview.Stack
+{
+    public class StackFacts
+    {
+        [Fact]
+        public void PushIncrementsLength()
+        {
+            var sut = new Stack();
+            sut.Push(1);
+            sut.Push(2);
+            Assert.Equal(2, sut.Count);
+        }
+
+        [Fact]
+        public void CannotPushMoreThanTenNumbers()
+        {
+            var sut = new Stack();
+            sut.Push(1);
+            sut.Push(2);
+            sut.Push(3);
+            sut.Push(4);
+            sut.Push(5);
+            sut.Push(6);
+            sut.Push(7);
+            sut.Push(8);
+            sut.Push(9);
+            sut.Push(10);
+            Assert.Throws<InvalidOperationException>(() => sut.Push(11));
+        }
+
+        [Fact]
+        public void PopReturnsLastPushedNumber()
+        {
+            var sut = new Stack();
+            var expected = 1;
+            sut.Push(0);
+            sut.Push(expected);
+            var actual = sut.Pop();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CannotPopEmptyStack()
+        {
+            var sut = new Stack();
+            sut.Push(1);
+            sut.Push(2);
+            sut.Push(3);
+            sut.Pop();
+            sut.Pop();
+            sut.Pop();
+            Assert.Throws<InvalidOperationException>(() => sut.Pop());
+        }
+    }
+}
